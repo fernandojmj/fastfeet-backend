f9f960d6fe038c8bbc7f6e5adce228967b3bf345
Fernando <fernandojmj@gmail.com>
2020-04-21 19:56:22 -0300

    'heroku'

--- src/database/seeders/20200312035407-admin-user.js
+++ src/database/seeders/20200312035407-admin-user.js
@@ -3,21 +3,21 @@
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-  up: QueryInterface => {
+  up: (QueryInterface) => {
     return QueryInterface.bulkInsert(
       "users",
       [
         {
           name: "Distribuidora FastFeet",
           email: "admin@fastfeet.com",
-          password_hash: bcrypt.hashSync("123456", 8),
+          password_hash: bcrypt.hashSync("2020", 8),
           created_at: new Date(),
-          updated_at: new Date()
-        }
+          updated_at: new Date(),
+        },
       ],
       {}
     );
   },
 
-  down: () => {}
+  down: () => {},
 };
